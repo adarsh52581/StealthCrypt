9545b7d1149e5bcda92a6ac09cb13bd0e7e5465d
Saurav <75363095+Saurav200193@users.noreply.github.com>
2024-02-13 02:45:26 +0530

    Create sender.js

---
+++ Landing Pages/sender.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+import styled from "styled-components";
+
+function MyComponent(props) {
+  return (
+    <Div>
+      <Div2>
+        <Img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/c779e328a64b8d9d4d2082826a589a9734b4c48f930cba13c98e7501beca5eda?"
+        />
+        <Div3>StealthCrypt</Div3>
+      </Div2>
+      <Div4>
+        <Div5>
+          <Div6>Encrypt Data in Image</Div6>
+          <Div7>Cover Image</Div7>
+          <Div8>Data to Encrypt</Div8>
+          <Div9>Enter your data here...</Div9>
+          <Div10>Encrypt Data</Div10>
+        </Div5>
+      </Div4>
+    </Div>
+  );
+}
+
