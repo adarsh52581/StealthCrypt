f830e7eb6f4f52413a6de235f42c891aa0473178
Adarsh N <adarshn5225@gmail.com>
2024-02-13 09:06:33 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('activate/<uidb64>/', views.activate, name='activate'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-]
