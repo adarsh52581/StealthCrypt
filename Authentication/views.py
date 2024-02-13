cbfbafbc59e8d32f49cb1ffc7365d24af74d1f89
Adarsh N <adarshn5225@gmail.com>
2024-02-13 09:08:49 +0530

    Add files via upload

---
+++ Authentication/views.py
@@ -0,0 +1,69 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from .forms import CustomUserCreationForm
+from .models import CustomUser
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('authentication/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'authentication/register.html', {'form': form})
+
+def activate(request, uidb64):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = CustomUser.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+        user = None
+    if user is not None:
+        user.is_verified = True
+        user.save()
+        login(request, user)
+        return redirect('home')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user.is_verified:
+                login(request, user)
+                return redirect('home')
+            else:
+                return HttpResponse('Your account is not verified yet!')
+        else:
+            return HttpResponse('Invalid login credentials!')
+    return render(request, 'authentication/login.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('home')
